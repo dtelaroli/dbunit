e0d163207b42fc23127510f5eb04113131f01aa4
Denilson Telaroli <ddts80@gmail.com>
2014-10-19 17:56:13 -0200

    Beans and Pagination params

--- plus-core/src/main/java/br/com/caelum/vraptor/plus/action/DefaultPaginateAllAction.java
+++ plus-core/src/main/java/br/com/caelum/vraptor/plus/action/DefaultPaginateAllAction.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.plus.api.Database;
 import br.com.caelum.vraptor.plus.api.action.PaginateAction;
 
 public class DefaultPaginateAllAction implements PaginateAction {
 
 	private final Database db;
+	private final Result result;
 	private int first = 0;
 	private int limit = 20;
 
@@ -19,12 +21,13 @@ public class DefaultPaginateAllAction implements PaginateAction {
 	 * @deprecated CDI eyes-only
 	 */
 	protected DefaultPaginateAllAction() {
-		this(null);
+		this(null, null);
 	}
 	
 	@Inject
-	public DefaultPaginateAllAction(Database db) {
+	public DefaultPaginateAllAction(Database db, Result result) {
 		this.db = db;
+		this.result = result;
 	}
 
 	@Override
@@ -34,12 +37,14 @@ public class DefaultPaginateAllAction implements PaginateAction {
 
 	@Override
 	public PaginateAction first(int first) {
+		result.include("first", first);
 		this.first = first;
 		return this;
 	}
 
 	@Override
 	public PaginateAction limit(int limit) {
+		result.include("limit", limit);
 		this.limit = limit;
 		return this;
 	}
--- plus-core/src/test/java/br/com/caelum/vraptor/plus/action/DefaultPaginateActionTest.java
+++ plus-core/src/test/java/br/com/caelum/vraptor/plus/action/DefaultPaginateActionTest.java
@@ -2,6 +2,7 @@ package br.com.caelum.vraptor.plus.action;
 
 import static br.com.caelum.vraptor.plus.api.Databases.find;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
@@ -15,25 +16,30 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.plus.MyModel;
 import br.com.caelum.vraptor.plus.api.Database;
 import br.com.caelum.vraptor.plus.api.action.PaginateAction;
 import br.com.caelum.vraptor.plus.api.db.FindDb;
+import br.com.caelum.vraptor.util.test.MockResult;
 
 public class DefaultPaginateActionTest {
 
 	private PaginateAction act;
 	@Mock private Database db;
 	@Mock private FindDb findDb;
+	private Result result;
 	
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		
+		result = new MockResult();
+		
 		when(findDb.findPaginate(MyModel.class, 10, 10)).thenReturn(Arrays.asList(new MyModel()));
 		when(db.use(find())).thenReturn(findDb);
 		
-		act = new DefaultPaginateAllAction(db);
+		act = new DefaultPaginateAllAction(db, result);
 	}
 
 	@Test
@@ -41,6 +47,13 @@ public class DefaultPaginateActionTest {
 		List<MyModel> all = act.first(10).limit(10).all(MyModel.class);
 		assertThat(all, not(empty()));
 		assertThat(all.get(0), instanceOf(MyModel.class));
+	}
+	
+	@Test
+	public void shouldIncludeParameters() {
+		act.first(10).limit(5);
+		assertThat(result.included().get("first"), equalTo(10));
+		assertThat(result.included().get("limit"), equalTo(5));
 	}
 
 }
